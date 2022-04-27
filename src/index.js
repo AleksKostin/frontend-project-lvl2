ed95d01e0968d10fd2b6ed6fabc47f08a65d8e4f
Aleksey Kostin <alekkostin91@gmail.com>
2022-04-28 01:52:32 +0300

    completed the second step of the project

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const getGenDiffHelp = () => {
   program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference')
-    .version('0.1.0');
-
+    .version('0.1.0')
+    .argument('<filepath1> <filepath2>')
+    .option('-f, --format <type>', 'output format')
+    .helpOption('-h, --help', 'output usage information');
   program.parse();
 };
 
