c04e9d2685cd872fd7f063ad8515eb9bdcdff3b9
Aleksey Kostin <alekkostin91@gmail.com>
2022-06-15 16:54:49 +0300

    refactoring

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import getParseFile from './src/parsers.js';
 import { getAstDiff } from './src/utils.js';
 import formattedDiff from './src/formatters/index.js';
 
-const genDiff = (filePath1, filePath2, formatter) => {
+const genDiff = (filePath1, filePath2, formatter = 'stylish') => {
   const data1 = getParseFile(filePath1);
   const data2 = getParseFile(filePath2);
   const diff = getAstDiff(data1, data2);
